85ba6ba0367bd75a51391a7dba3d8edf14d3462f
kanon <60179867+decryp2kanon@users.noreply.github.com>
2020-10-07 06:05:08 -0400

    lint-python

--- test/functional/p2p_dos_header_tree.py
+++ test/functional/p2p_dos_header_tree.py
@@ -8,10 +8,11 @@ from test_framework.messages import (
     CBlockHeader,
     FromHex,
 )
-from test_framework.p2p import (
-    P2PInterface,
-    msg_headers,
-)
+# TODO.ZENY.POW # TEST DISABLED
+# from test_framework.p2p import (
+#     P2PInterface,
+#     msg_headers,
+# )
 from test_framework.test_framework import BitcoinTestFramework
 
 import os
@@ -46,7 +47,7 @@ class RejectLowDifficultyHeadersTest(BitcoinTestFramework):
         self.headers_fork = [FromHex(CBlockHeader(), h) for h in self.headers_fork]
 
         self.log.info("Feed all non-fork headers, including and up to the first checkpoint")
-        peer_checkpoint = self.nodes[0].add_p2p_connection(P2PInterface())
+        # peer_checkpoint = self.nodes[0].add_p2p_connection(P2PInterface()) # TODO.ZENY.POW # TEST DISABLED
 
         # TODO.ZENY.POW # TEST DISABLED
         """
