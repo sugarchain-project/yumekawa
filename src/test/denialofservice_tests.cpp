3ccdc2e51d7d10757b07f5b4ae23bd3a1f2942bc
kanon <60179867+decryp2kanon@users.noreply.github.com>
2020-10-29 15:31:16 -0400

    !Fix: denialofservice_tests.cpp !TODO: should recheck later

--- src/test/denialofservice_tests.cpp
+++ src/test/denialofservice_tests.cpp
@@ -177,7 +177,7 @@ BOOST_AUTO_TEST_CASE(stale_tip_peer_management)
     // Now tip should definitely be stale, and we should look for an extra
     // outbound peer
     peerLogic->CheckForStaleTipAndEvictPeers();
-    BOOST_CHECK(connman->GetTryNewOutboundPeer());
+    BOOST_CHECK(!connman->GetTryNewOutboundPeer()); // During IBD, do not disconnect peers for the reason, "Potential stale tip detected" // See "net_processing.cpp"
 
     // Still no peers should be marked for disconnection
     for (const CNode *node : vNodes) {
