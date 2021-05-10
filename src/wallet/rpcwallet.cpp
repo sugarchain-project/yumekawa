f7de0267f8deda440d139d00525bc0d7fd72ef04
kanon <60179867+decryp2kanon@users.noreply.github.com>
2021-05-10 08:22:53 -0400

    a word

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -4532,7 +4532,7 @@ static RPCHelpMan walletdisplayaddress()
     return RPCHelpMan{"walletdisplayaddress",
         "Display address on an external signer for verification.",
         {
-            {"address",     RPCArg::Type::STR, RPCArg::Optional::NO, /* default_val */ "", "bitcoin address to display"},
+            {"address",     RPCArg::Type::STR, RPCArg::Optional::NO, /* default_val */ "", "sugarchain address to display"},
         },
         RPCResult{
             RPCResult::Type::OBJ,"","",
